716157240ccf63850d245c167e23b80ab73a3790
BrHugo662 <hu2712go@gmail.com>
2025-09-04 16:49:12 +0200

    Q4.6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,6 +1,6 @@
 def puissance(a,b):
-	if a==0 or b==0:
-		return ('erreur, car un nombre est egale a 0')
+	if a>0:
+		return ('erreur')
 	if not type(a) is int or not type(b) is int:
 		return ("Ce n'est pas un nombre entier")
 	else:
