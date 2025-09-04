b4f7f056169af7a420fb58a888444d22e0baf2b8
BrHugo662 <hu2712go@gmail.com>
2025-09-04 17:23:10 +0200

    Q4.6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,8 @@
 def puissance(a,b):
 	if not type(a) is int or not type(b) is int:
 		return ("Ce n'est pas un nombre entier")
+	if a==0:
+		if b<0:
+			return('0 avec une puissance négative imposible')
 	else:
 		return(a**b)
